245dd26bd2ec0b89e2caaae659a196c59d7a8589
linbingfeng <linbingfeng@foxmail.com>
2017-05-28 19:13:21 +0800

    项目大更新

--- trip_data/settings.py
+++ trip_data/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/site_media/'
 STATIC_ROOT = os.path.join(BASE_DIR,'site_media')
 STATICFILES_DIRS = (
      os.path.join(BASE_DIR, 'app/site_media'),
